b736150792ac0cdc2ac5534d5db4b6226cb4c69c
balwanti12 <balwantimane123@gmail.com>
2020-04-03 16:53:29 +0530

    vyapar app

---
+++ app/src/main/java/com/example/salereport/Add_sale.java
@@ -0,0 +1,32 @@
+package com.example.salereport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Add_sale extends AppCompatActivity {
+
+    EditText name;
+    EditText total;
+    Button save;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_sale);
+       name= findViewById(R.id.editText6);
+       total=findViewById(R.id.editText7);
+       save=findViewById(R.id.button2);
+
+       save.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Toast.makeText(getApplicationContext(),"saved successfully",Toast.LENGTH_SHORT).show();
+           }
+       });
+    }
+}
---
+++ app/src/main/java/com/example/salereport/MainActivity.java
@@ -0,0 +1,139 @@
+package com.example.salereport;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    Button addsale;
+    TextView from;
+    TextView to;
+    Spinner spin1;
+    Spinner spin2;
+    EditText fromdate;
+    EditText todate;
+    DatePickerDialog datePickerDialog;
+    DatePickerDialog datePickerDialog1;
+    ArrayAdapter arrayAdapter1;
+    ArrayAdapter arrayAdapter2;
+        String[] s1={"Today","This week","This month","This quarter","This financial year","custom" };
+        String[] s2={"Sale & Cr.Note","Sale","Credit Note"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addsale=findViewById(R.id.button);
+        addsale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),Add_sale.class);
+                startActivity(i);
+            }
+        });
+        from=findViewById(R.id.textView3);
+        to=findViewById(R.id.textView4);
+        todate=findViewById(R.id.editText4);
+         fromdate=findViewById(R.id.editText3);
+         fromdate.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                Calendar calendar=Calendar.getInstance();
+                final int year1=calendar.get(Calendar.YEAR);
+                 final int month1=calendar.get(Calendar.MONTH);
+                 final int day1=calendar.get(Calendar.DAY_OF_MONTH);
+
+               datePickerDialog=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                   @Override
+                   public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                     fromdate.setText(day1+"-"+month1+"-"+year1);
+                   }
+               },year1,month1,day1);
+               datePickerDialog.show();
+             }
+         });
+        fromdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar=Calendar.getInstance();
+                final int year1=calendar.get(Calendar.YEAR);
+                final int month1=calendar.get(Calendar.MONTH);
+                final int day1=calendar.get(Calendar.DAY_OF_MONTH);
+
+                datePickerDialog=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        fromdate.setText(day1+"-"+month1+"-"+year1);
+                    }
+                },year1,month1,day1);
+                datePickerDialog.show();
+            }
+        });
+        todate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar1=Calendar.getInstance();
+                final int year2=calendar1.get(Calendar.YEAR);
+                final int month2=calendar1.get(Calendar.MONTH);
+                final int day2=calendar1.get(Calendar.DAY_OF_MONTH);
+
+                datePickerDialog1=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        todate.setText(day2+"-"+month2+"-"+year2);
+                    }
+                },year2,month2,day2);
+                datePickerDialog1.show();
+            }
+        });
+
+
+        spin1=findViewById(R.id.spinner);
+        arrayAdapter1 = new ArrayAdapter(this,android.R.layout.simple_spinner_item,s1);
+        arrayAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin1.setAdapter(arrayAdapter1);
+
+        spin2=findViewById(R.id.spinner2);
+        arrayAdapter2 = new ArrayAdapter(this,android.R.layout.simple_spinner_item,s2);
+        arrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin2.setAdapter(arrayAdapter2);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.commonmenu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+         int id=item.getItemId();
+         if(id == R.id.first) {
+             Toast.makeText(getApplicationContext(), "access to pdf", Toast.LENGTH_SHORT).show();
+         }
+         if(id==R.id.second )
+         {
+             Toast.makeText(getApplicationContext(), "access to excel", Toast.LENGTH_SHORT).show();
+         }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
